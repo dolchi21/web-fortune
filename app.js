8aa423e61c0630a17088d6544ebfee8d1a4c1260
Axel Dolce <axel.dolce@amtek.com.ar>
2018-02-05 14:59:38 -0300

    refactor

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 var express = require('express')
 var uuid = require('uuid')
 
-var UUID = uuid.v4()
-
 var fortune = require('./fortune')
+
+var UUID = uuid.v4()
 
 var app = express()
 
 app.use((req, res, next) => {
-    res.response = {
-        meta: {
-            uuid: UUID
-        }
+    var response = {
+        meta: {}
     }
-    res.data = val => res.response.data = val
+    res.meta = (key, value) => response.meta[key] = value
+    res.data = val => response.data = val
+    res.response = () => response
+    next()
+})
+
+app.use((req, res, next) => {
+    res.meta('uuid', UUID)
     next()
 })
 
 app.get('/fortune', (req, res, next) => Promise.resolve().then(async () => {
     res.data(fortune())
-    res.json(res.response)
+    res.json(res.response())
 }))
 
 module.exports = app
