c4e6e62eba43bb9b6ce892a8504711488aaac760
Axel Dolce <axel.dolce@amtek.com.ar>
2018-02-05 14:48:27 -0300

    json api

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express')
+var uuid = require('uuid')
+
+var UUID = uuid.v4()
 
 var fortune = require('./fortune')
 
 var app = express()
 
+app.use((req, res, next) => {
+    res.response = {
+        meta: {
+            uuid: UUID
+        }
+    }
+    res.data = val => res.response.data = val
+    next()
+})
+
 app.get('/fortune', (req, res, next) => Promise.resolve().then(async () => {
-    res.json({
-        data: fortune()
-    })
+    res.data(fortune())
+    res.json(res.response)
 }))
 
 module.exports = app
